7ea9deef6b4ff50dbe4712fc217e667e180da44a
wdd1128 <wdd1128@naver.com>
2021-03-11 16:01:53 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+ pw = document.querySelector("#pw"),
+ loginbtn = document.querySelector("button");
+
+ loginbtn.addEventListener("click",login);
+
+ function login(){
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    }
+    console.log(req);
+ }
