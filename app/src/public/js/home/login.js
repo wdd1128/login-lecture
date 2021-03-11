e828b48bdf3e7ab1f2a45cb973a631b6bfccef3c
wdd1128 <wdd1128@naver.com>
2021-03-11 16:09:09 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,4 +12,12 @@ const id = document.querySelector("#id"),
         pw: pw.value,
     }
     console.log(req);
- }
+
+    fetch("/login",{
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(req),
+    })
+ };
